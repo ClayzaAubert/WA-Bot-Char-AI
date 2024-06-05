93fa0517412be055aed2011fdd56ebf262b1a367
Clayza <92805294+ClayzaAubert@users.noreply.github.com>
2024-06-05 11:00:55 +0700

    Update gptrp.js

--- gptrp.js
+++ gptrp.js
@@ -1,23 +1,23 @@
-const axios = require("axios");
+const axios = require('axios');
 
-const CharID = "CharID";
-const ChatID = "ChatID";
-const ApiKey = "Apikey https://api.maelyn.my.id";
+const CHAR_ID = 'FRdKHnLG4JF6I14PexPWFvVyfrp-nZi6NFcoljmPPPM';
+const CHAT_ID = 'b1b68569-031d-4a64-a8ad-0878cae0fd59';
+const API_KEY = 'YOUR_APIKEY';
+const BASE_URL = 'https://api.maelyn.my.id/api/cai-chat';
 
 async function MealynAPI(query) {
+    const apiUrl = `${BASE_URL}?q=${encodeURIComponent(query)}&charid=${CHAR_ID}&chatid=${CHAT_ID}&apikey=${API_KEY}`;
+
     try {
-        const apiUrl = `https://api.maelyn.my.id/api/cai-chat?q=${encodeURIComponent(query)}&charid=${CharID}&chatid=${CharID}&apikey=${ApiKey}`;
         const response = await axios.get(apiUrl, {
-            headers: {
-                'Content-Type': 'application/json'
-            }
+            headers: { 'Content-Type': 'application/json' },
         });
 
         return response.data.result.candidates[0].raw_content;
     } catch (error) {
-        console.error("API Call Error:", error);
+        console.error('API Call Error:', error);
         throw error;
     }
 }
 
-module.exports = MealynAPI
+module.exports = MaelynAPI
